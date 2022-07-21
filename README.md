969b21009bf2eb15a89d21b17d0fe443a421de8e
pilate <pilatech6@gmail.com>
2022-07-21 09:38:13 +0200

    added desktop styles

---
+++ README.md
@@ -0,0 +1,55 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+
+## Overview
+
+Time tracking dashboard using plain HTML, CSS and Vanilla JavaScript
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: [here](https://github.com/pilatech/time-tracking-dashboard)
+- Live Site URL: [here](https://time-tracking-dashboard-by-pilate.netlify.app/)
+
+## My process
+
+- First created the markup with emphasis on semantics - my approach is 'desktop first' when it comes to marking up my documents even though I do peep into mobile layout here and there.
+- Created CSS for mobile version
+- Then I jumped into creating JavaScript for interactivity
+- I then got back into completing the desktop styles
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [@pilatech](https://www.frontendmentor.io/profile/pilatech)
+- Twitter - [@pchinyengetere](https://www.twitter.com/pchinyengetere)
--- script.js
+++ script.js
@@ -1,76 +1,42 @@
-
-
 let currentTimeFrame = 'Weekly'
+// this is the state that allows us to change our UI based off of
 
-function removeOtherActive(){
- document.querySelectorAll('.profile__time').forEach(el => {
-  el.classList.remove('profile__time--active')
- })
-}
+paintUI(currentTimeFrame) 
+// render UI with 'Weekly as the initial timeframe
 
 document.addEventListener('click', (event) => {
   el = event.target
   if(el.classList.contains('profile__time')){
-   removeOtherActive()
+   removeOtherActiveStates()
    currentTimeFrame = el.textContent
    el.classList.add('profile__time--active')
    paintUI(currentTimeFrame)
   }
 })
 
-
 async function getData(){
  const res = await fetch('./data.json')
  const data = await res.json()
  return data
-
 }
 
-paintUI(currentTimeFrame)
-
-// getData().then(data => {
-
-//  const container = document.querySelector('.container')
-
-//  const cardMarkupArr = data.map(item => {
-
-//   const cardClass = `card--${item.title.toLowerCase().split(' ').join('-')}`
-
-//   return(
-//    `
-//    <article class="card ${cardClass}">
-//    <header class="card__hero">
-//    </header>
-//    <footer class="card__footer">
-//      <h2 class="card__title">
-//       ${item.title}
-//       <small class="card__title--accent">${item.timeframes.weekly.current}hrs</small>
-//      </h2>
-//      <aside class="card__extra-detail">
-//        <img src="images/icon-ellipsis.svg" alt="ellipsis" class="card__icon">
-//        <p class="card__time">Last Week - ${item.timeframes.weekly.previous}hrs</p>
-//      </aside>
-//    </footer>
-//   </article>
-//    `
-//   )
-//  })
-
-//  container.innerHTML += cardMarkupArr.join()
-
-// })
-
+function removeOtherActiveStates(){
+ document.querySelectorAll('.profile__time').forEach(el => {
+  el.classList.remove('profile__time--active')
+ })
+}
 
 function paintUI(timeFrame){
-
+// so the timeFrame params allows us to filter data returned from our fetch
  getData().then(data => {
-
   const content = document.querySelector('.content')
- 
-  const cardMarkupArr = data.map(item => {
- 
+  const cardMarkupArray = data.map(item => {
    const cardClass = `card--${item.title.toLowerCase().split(' ').join('-')}`
- 
+   const currentTime = item.timeframes[timeFrame.toLowerCase()].current
+   const previousTime = item.timeframes[timeFrame.toLowerCase()].previous
+   const timeLable = timeFrame.split('').slice(0, -2).join('')
+   const time = timeLable === 'Dai' ? 'Day' : timeLable
+
    return(
     `
     <article class="card ${cardClass}">
@@ -79,11 +45,11 @@ function paintUI(timeFrame){
     <footer class="card__footer">
       <h2 class="card__title">
        ${item.title}
-       <small class="card__title--accent">${item.timeframes[timeFrame.toLowerCase()].current}hrs</small>
+       <small class="card__title--accent">${currentTime} ${currentTime === 1 ? "hr" : "hrs"}</small>
       </h2>
       <aside class="card__extra-detail">
         <img src="images/icon-ellipsis.svg" alt="ellipsis" class="card__icon">
-        <p class="card__time">Last ${timeFrame.split('').slice(0, -2).join('')} - ${item.timeframes[timeFrame.toLowerCase()].previous}hrs</p>
+        <p class="card__time">Last ${time} - ${previousTime} ${previousTime === 1 ? "hr" : "hrs"}</p>
       </aside>
     </footer>
    </article>
@@ -91,7 +57,7 @@ function paintUI(timeFrame){
    )
   })
  
-  content.innerHTML = cardMarkupArr.join()
+  content.innerHTML = cardMarkupArray.join('')
  
  })
 
